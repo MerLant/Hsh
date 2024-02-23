29464f174cf2b1102c1dd008cf52ee1f23ee2eec
MerLant <merlantpc@gmail.com>
2024-02-24 02:21:57 +0500

    Update BD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import Image from "next/image";
+
 import styles from "./page.module.css";
 import CodeMirror from '@uiw/react-codemirror';
 import {langs} from '@uiw/codemirror-extensions-langs';
